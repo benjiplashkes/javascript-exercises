33398a2aacccf75cad750522791ebe7b31ef0c6d
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-02-23 15:14:31 +0200

    Add logic to removeFromArray function
    Loop arguments and filter the array based on values entered.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...valuesToRemove) {
+   valuesToRemove.forEach(value => {
+    array = array.filter(item => item !== value)
+   }
+   )
+   return array
 };
 
 // Do not edit below this line
