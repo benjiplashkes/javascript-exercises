0f060c076d0c0a64171e035fc00e895672e7889d
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-02-25 11:39:30 +0200

    Add Functions logic to both functions
    Added convertion formula to both functions -
    convertToFahrenheit and convertToCelsius
    
    added logic to return one decimal place in the return value of the
    formulas.
    
    all tests pass!
    
     - exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+  const degC = (degF - 32) * 0.5556
+  return parseFloat(degC.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  const degF = degC * 1.8 + 32
+  return parseFloat(degF.toFixed(1))
 };
 
 // Do not edit below this line
