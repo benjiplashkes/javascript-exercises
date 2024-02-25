88d35de399e1db9dc2a5c3f7779e4ca3ba0895db
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-02-25 10:41:46 +0200

    Add Logic to sumAll function
    Finish exercise 05_sumAll.
    added basic function logic.
    added type cheching end error ststes on invalid input.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,7 @@
 const sumAll = function(from, to) {
+    if(to < 0 || from < 0) return "ERROR"
+    if(typeof to !== "number" || typeof from !== "number") return "ERROR"
+
     const max = Math.max(from, to)
     let min = Math.min(from, to)
     let result = 0
